7c191145d09b41f3bae1741f9a9b4e97a816b755
Florian Probst <derbalvald@gmail.com>
2022-08-23 10:49:51 +0200

    Added AST prefix to plugin specific config variables and AST_state
    now using "AST" prefix to avoid collisions in config variables.
    Added alternative way to show info in a single line with "AST_state"
    -> still needs preferences so User can turn on and off which info the plugin should show.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ArtemisScannerTracker
  A EDMC plugin that keeps track of the Exobiology Scanner of a CMDRs Artemis suit in ED:Odyssey
+
+## Installation
+
+1. Find your EDMC _plugins_ folder (In EDMC settings under _plugins_ you can click the button _Open_ to open the _plugins_ folder)
+2. Download the latest release.
+3. Unpack the _.zip_ archive of the latest release and move the _ArtemisScannerTracker_ folder into said _plugins_ folder.
+
+You will need to re-start EDMC if EDMC was open while you installed the plugin. Otherwise just start EDMC
+
+## Usage
+
+You have to do _nothing_ (yay) apart from having to play Elite Dangerous while using this plugin with EDMC.
+
+The plugin keeps track of the ArtemisScannerState through a Cmdr's journal entries that relais the information, which type of exobiological life the last scan was (Tussock, Bacteria, Aleoida, Stratum, Fungoida, etc. with corresponding colour variant.) where the last scan occurred (Star system and specific body within the system, (exact position's of former scans for a certain type of exobiological life to be added later)) and the current progression with the last exobiological scan. (If theres nothing on the Scanner, or if we got 1/3 Scans or 2/3 Scans done.) So you don't have to look at the Scanner led in game after returning to a session and wonder "Which goddamn plant was I scanning again?" while being several thousand lightyears away from Sol. Sadly that type of information isn't around so you can just look it up on dunno the Scanner. The one thing that even despite not telling me _what_ or _where_ I scanned still telling me that my Scan is _incomplete_. The horror. Damn you Supratech! You damn Scoudrels!
+
+Anyway with this plugin those kind of problems are hopefully becoming a thing of the past for our all most favourite recreational activity of scanning space grass while binging Star Trek TNG I guess.
--- load.py
+++ load.py
@@ -1,11 +1,10 @@
 """
-Example EDMC plugin.
-It adds a single button to the EDMC interface that displays the number of times it has been clicked.
+Artemis Scanner Tracker by Balvald
+created from the EDMC example plugin.
 """
 
-# TODO modify implementation to have a basic tracker for the current state of the Artemis' suit scanner.
-
 import logging
+from pickle import FALSE
 import tkinter as tk
 from typing import Optional
 
@@ -15,27 +14,22 @@ from config import appname, config
 # This **MUST** match the name of the folder the plugin is in.
 PLUGIN_NAME = "ArtemisScannerTracker"
 
+# Shows debug fields in preferences when True
+debug = False
+
 logger = logging.getLogger(f"{appname}.{PLUGIN_NAME}")
 
 class ArtemisScannerTracker:
-    """
-    First iteration is still the example Click counter plugin.
-
-    Observation: the value that the plugin tracks is being saved and retrieved wenn closing and opening EDMC! that great yay C:
-
-    ClickCounter implements the EDMC plugin interface.
-    It adds a button to the EDMC UI that displays the number of times it has been clicked, and a preference to set
-    the number directly.
-    """
 
     def __init__(self) -> None:
         # Be sure to use names that wont collide in our config variables
-        self.last_scan_plant: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('last_scan_plant')))
-        self.last_scan_system: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('last_scan_system')))
-        self.last_scan_body: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('last_scan_body')))
-        self.current_scan_progress: Optional[tk.StringVar] = tk.StringVar(value=(str(config.get_int('current_scan_progress')) + str("/3")))
-        self.current_system: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('current_system')))
-        self.current_body: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('current_body')))
+        self.AST_last_scan_plant: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_last_scan_plant')))
+        self.AST_last_scan_system: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_last_scan_system')))
+        self.AST_last_scan_body: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_last_scan_body')))
+        self.AST_current_scan_progress: Optional[tk.StringVar] = tk.StringVar(value=(str(config.get_int('AST_current_scan_progress')) + str("/3")))
+        self.AST_current_system: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_current_system')))
+        self.AST_current_body: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_current_body')))
+        self.AST_state: Optional[tk.StringVar] = tk.StringVar(value=str(config.get_str('AST_state')))
         logger.info("ArtemisScannerTracker instantiated")
 
     def on_load(self) -> str:
@@ -65,22 +59,26 @@ class ArtemisScannerTracker:
         current_row = 0
         frame = nb.Frame(parent)
 
-        # setup debug fields for the scanner.
-        nb.Label(frame, text='Species').grid(row=current_row)
-        nb.Entry(frame, textvariable=self.last_scan_plant).grid(row=current_row, column=1)
-        current_row += 1  # Always increment our row counter, makes for far easier tkinter design.
-        
-        nb.Label(frame, text='System of last Scan').grid(row=current_row)
-        nb.Entry(frame, textvariable=self.last_scan_system).grid(row=current_row, column=1)
-        current_row += 1
-        
-        nb.Label(frame, text='Body of last Scan').grid(row=current_row)
-        nb.Entry(frame, textvariable=self.last_scan_body).grid(row=current_row, column=1)
-        current_row += 1
+        # TODO create useful preferences for the plugin: i.e. tick boxes to show or hide certain info like current system and body and a Reset state button.
+        # and remove the debug fields, where the user could just put anything in.
 
-        nb.Label(frame, text='Scan progress').grid(row=current_row)
-        nb.Entry(frame, textvariable=self.current_scan_progress).grid(row=current_row, column=1)
-        current_row += 1
+        if debug:
+            # setup debug fields for the scanner.
+            nb.Label(frame, text='Species').grid(row=current_row)
+            nb.Entry(frame, textvariable=self.AST_last_scan_plant).grid(row=current_row, column=1)
+            current_row += 1  # Always increment our row counter, makes for far easier tkinter design.
+            
+            nb.Label(frame, text='System of last Scan').grid(row=current_row)
+            nb.Entry(frame, textvariable=self.AST_last_scan_system).grid(row=current_row, column=1)
+            current_row += 1
+            
+            nb.Label(frame, text='Body of last Scan').grid(row=current_row)
+            nb.Entry(frame, textvariable=self.AST_last_scan_body).grid(row=current_row, column=1)
+            current_row += 1
+
+            nb.Label(frame, text='Scan progress').grid(row=current_row)
+            nb.Entry(frame, textvariable=self.AST_current_scan_progress).grid(row=current_row, column=1)
+            current_row += 1
 
         return frame
 
@@ -91,12 +89,12 @@ class ArtemisScannerTracker:
         :param cmdr: The current ED Commander
         :param is_beta: Whether or not EDMC is currently marked as in beta mode
         """
-        # You need to cast to `int` here to store *as* an `int`, so that
-        # `config.get_int()` will work for re-loading the value.
-        config.set('current_scan_progress', int(self.current_scan_progress.get()[0])) # type: ignore
-        config.set('last_scan_system', str(self.last_scan_system.get()))
-        config.set('last_scan_body', str(self.last_scan_body.get()))
-        config.set('last_scan_plant', str(self.last_scan_plant.get()))
+        config.set('AST_current_scan_progress', int(self.AST_current_scan_progress.get()[0]))
+        config.set('AST_last_scan_system', str(self.AST_last_scan_system.get()))
+        config.set('AST_last_scan_body', str(self.AST_last_scan_body.get()))
+        config.set('AST_last_scan_plant', str(self.AST_last_scan_plant.get()))
+        config.set('AST_state', str(self.AST_state.get()))
+        logger.info("ArtemisScannerTracker saved")
 
     def setup_main_ui(self, parent: tk.Frame) -> tk.Frame:
         """
@@ -107,71 +105,88 @@ class ArtemisScannerTracker:
         """
         current_row = 12
         frame = tk.Frame(parent)
+        """
+        old buttons
         button = tk.Button(
             frame,
             text="Increment",
-            command=lambda: self.current_scan_progress.set(str((int(self.current_scan_progress.get()[0]) + 1) % 4) + str("/3"))  # type: ignore
+            command=lambda: self.AST_current_scan_progress.set(str((int(self.AST_current_scan_progress.get()[0]) + 1) % 4) + str("/3"))
         )
         resetbutton = tk.Button(
             frame,
             text="Reset",
-            command=lambda: self.current_scan_progress.set(str("0/3"))
-              # type: ignore
+            command=lambda: self.AST_current_scan_progress.set(str("0/3"))
         )
         #button.grid(row=current_row) # commented out -> no button to be shown
         current_row -= 1
         #resetbutton.grid(row=current_row)
         current_row -= 1
+        """
         tk.Label(frame, text="Current Body:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.current_body).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_current_body).grid(row=current_row, column=1)
         current_row -= 1
         tk.Label(frame, text="Current System:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.current_system).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_current_system).grid(row=current_row, column=1)
         current_row -= 1
         tk.Label(frame, text="Body of last Scan:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.last_scan_body).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_last_scan_body).grid(row=current_row, column=1)
         current_row -= 1
         tk.Label(frame, text="System of last Scan:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.last_scan_system).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_last_scan_system).grid(row=current_row, column=1)
         current_row -= 1
         tk.Label(frame, text="Scan Progress:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.current_scan_progress).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_current_scan_progress).grid(row=current_row, column=1)
         current_row -= 1
         tk.Label(frame, text="Species:").grid(row=current_row)
-        tk.Label(frame, textvariable=self.last_scan_plant).grid(row=current_row, column=1)
+        tk.Label(frame, textvariable=self.AST_last_scan_plant).grid(row=current_row, column=1)
+        current_row -= 1
+        tk.Label(frame, text="Last Exobioligy Scan:").grid(row=current_row)
+        tk.Label(frame, textvariable=self.AST_state).grid(row=current_row, column=1)
         return frame
-
-    def increment_scan_progress(self):
-        self.current_scan_progress.set(str((int(self.current_scan_progress.get()[0]) + 1) % 4) + str("/3"))
-
     
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-        """
-        react accordingly to ScanOrganic event
-        :param cmdr: The current ED Commander
-        :param is_beta: Whether or not EDMC is currently marked as in beta mode
-        :param system: ?
-        :param station: ?
-        :param entry: the current Journal entry
-        :param state: ?
-        """
-        if entry["event"] in ["Location", "Embark", "Disembark", "Touchdown", "Liftoff", "FSDJump"]:
-            # Get current system name and body from events that needs to happen.
-            plugin.current_system.set(entry["StarSystem"])
-            plugin.current_body.set(entry["Body"])
-        
-        if entry["event"] == "ScanOrganic":
-            plugin.last_scan_plant.set(entry["Species_Localised"])
-            plugin.last_scan_system.set(plugin.current_system.get())
-            plugin.last_scan_body.set(plugin.current_body.get())
-            if entry["ScanType"] == "Log":
-                plugin.current_scan_progress.set("1/3")
-            elif entry["ScanType"] == "Sample":
-                plugin.current_scan_progress.set("2/3")
-            elif entry["ScanType"] == "Analyse":
-                plugin.current_scan_progress.set("3/3")
-            else:
-                plugin.current_scan_progress.set("Excuse me what the fuck") #Somethings horibly wrong if we end up here
+    """
+    react accordingly to ScanOrganic event
+    :param cmdr: The current ED Commander
+    :param is_beta: Whether or not EDMC is currently marked as in beta mode
+    :param system: current system? unused
+    :param station: unused
+    :param entry: the current Journal entry
+    :param state: unused
+    """
+
+    flag = False
+
+    if entry["event"] == "ScanOrganic":
+        flag = True
+        plugin.AST_last_scan_plant.set(entry["Species_Localised"])
+        # In the eventuality that the user started EMDC after the "Location" event happens and directly scans a plant
+        # these lines wouldn't be able to do anything but to set the System and body of the last Scan to "None"
+        plugin.AST_last_scan_system.set(plugin.AST_current_system.get())
+        plugin.AST_last_scan_body.set(plugin.AST_current_body.get())
+        if entry["ScanType"] == "Log":
+            plugin.AST_current_scan_progress.set("1/3")
+        elif entry["ScanType"] == "Sample":
+            plugin.AST_current_scan_progress.set("2/3")
+        elif entry["ScanType"] == "Analyse":
+            plugin.AST_current_scan_progress.set("3/3")
+        else:
+            plugin.AST_current_scan_progress.set("Excuse me what the fuck") #Somethings horibly wrong if we end up here
+
+    if entry["event"] in ["Location", "Embark", "Disembark", "Touchdown", "Liftoff", "FSDJump"]:
+        flag = True
+        # Get current system name and body from events that needs to happen.
+        plugin.AST_current_system.set(entry["StarSystem"])
+        plugin.AST_current_body.set(entry["Body"])
+        # To fix the aforementioned eventuality where the systems end up being "None" we update the last scan location
+        # When the CMDR gets another journal entry that tells us the players location.
+        if ((plugin.AST_last_scan_system.get() == "None") or (plugin.AST_last_scan_body.get() == "None")):
+            plugin.AST_last_scan_system.set(entry["StarSystem"])
+            plugin.AST_last_scan_body.set(entry["Body"])
+
+    if flag:
+        # we changed a value so we update line.
+        plugin.AST_state.set(plugin.AST_last_scan_plant.get() + " (" + plugin.AST_current_scan_progress.get() + ") on: " + plugin.AST_last_scan_body.get())
 
 
 plugin = ArtemisScannerTracker()
